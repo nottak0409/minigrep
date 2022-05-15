57718cbb96bc8ec30962b1dfeebbf7864dc603e7
nottak0409 <nottak0409@gmail.com>
2022-05-15 22:45:35 +0900

    [add]ファイルの中身を取り出す処理作成

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +10,9 @@ fn main() {
 
     println!("Search for {}", query);
     println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("ファイルが見つかりませんでした");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("ファイルの読み込み中に問題が発生しました");
+    println!("テキストは{}", contents);
 }
