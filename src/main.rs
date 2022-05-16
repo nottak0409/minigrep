b749dd1e8b597194cb701dde06d3cf0c9e7a7bf0
nottak0409 <nottak0409@gmail.com>
2022-05-16 17:23:19 +0900

    [add]構造体をクラス化して、関数をmethodとして紐づける

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Search for {}", config.query);
     println!("In file {}", config.filename);
@@ -20,9 +20,12 @@ struct Config {
     query: String,
     filename: String,
 }
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
 
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
