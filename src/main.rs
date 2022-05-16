c940dcf5f47479b85c5b01ed369deb043e66cac7
nottak0409 <nottak0409@gmail.com>
2022-05-16 17:55:00 +0900

    [add]crateを分割

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,34 +17,8 @@ fn main() {
     println!("Search for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("エラーが発生しました {}", e);
         process::exit(1);
     }
-}
-
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("引数の数が足りません");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)?;
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-    println!("テキストは{}", contents);
-    Ok(())
 }
