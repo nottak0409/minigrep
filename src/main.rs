67807733c1bf4cb6ad2834502d4ee69f5922e452
nottak0409 <nottak0409@gmail.com>
2022-05-16 17:17:13 +0900

    [add]parse_configの返り値を構造体に変更

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,24 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Search for {}", query);
-    println!("In file {}", filename);
+    println!("Search for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("ファイルが見つかりませんでした");
+    let mut f = File::open(config.filename).expect("ファイルが見つかりませんでした");
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("ファイルの読み込み中に問題が発生しました");
     println!("テキストは{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-    (query, filename)
+    Config { query, filename }
 }
