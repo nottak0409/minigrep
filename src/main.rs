c930649a254a7f3e845cb25249f24d72565c3716
nottak0409 <nottak0409@gmail.com>
2022-05-18 08:17:33 +0900

    [add]エラー時のエラーメッセージを標準エラー出力に出すように処理を変更

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ fn main() {
 
     let config = Config::new(&args).unwrap_or_else(|err| {
         // 引数解析時に問題
-        println!("引数に問題があります: {}", err);
+        eprintln!("引数に問題があります: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("エラーが発生しました {}", e);
+        eprintln!("エラーが発生しました {}", e);
         process::exit(1);
     }
 }
