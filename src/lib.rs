2028ee78c0bee6f01fb334bbbdd6eacf8f038e9c
nottak0409 <nottak0409@gmail.com>
2022-05-18 15:43:35 +0900

    [add]search関数をイテレーターを用いて処理を簡略化

--- src/lib.rs
+++ src/lib.rs
@@ -39,32 +39,22 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         search_case_insensitive(&config.query, &contents)
     };
 
-    for line in search(&config.query, &contents) {
+    for line in results {
         println!("{}", line);
     }
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str)-> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines()
+    .filter(|line| line.contains(&query.to_lowercase()))
+    .collect()
 }
 
 #[cfg(test)]
